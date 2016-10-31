5443ec3743024839df012842aef4f7e8731930a9
Himanshu Bhardwaj <himanshu_bhardwajin@yahoo.co.in>
2016-10-31 18:00:34 +0530

    LinkedList more functions
    Delete Node at position
    Length of linkedList
    Swapping of Nodes
    Reversing a LinkedList
    SortedMerge of 2 LinkedLists

--- src/main/java/com/himanshu/poc/ds/linkedlist/LinkedList.java
+++ src/main/java/com/himanshu/poc/ds/linkedlist/LinkedList.java
@@ -37,6 +37,25 @@ public class LinkedList {
     }
   }
   
+  //It can be done through for-loop but trying it via recursion
+  private Node deleteAtPos(Node node, int currentPos, int posToDelete) {
+    if (currentPos == posToDelete) {
+      return node.getNext();
+    } else {
+      ++currentPos;
+      node.setNext(deleteAtPos(node.getNext(), currentPos, posToDelete));
+      return node;
+    }
+  }
+  
+  private int findLength(Node node) {
+    if (node == null) {
+      return 0;
+    } else {
+      return 1+findLength(node.getNext());
+    }
+  }
+  
   public void add(int data) {
     root = addLast(root, data);
   }
@@ -45,11 +64,107 @@ public class LinkedList {
     root = delete(root, data);
   }
   
+  public void deleteAtPos(int pos) {
+    root = deleteAtPos(root, 0, pos);
+  }
+  
+  public int length() {
+    return findLength(root);
+  }
+  
   @Override
   public String toString() {
     return "LinkedList [root=" + root + "]";
   }
-
+  
+  public void swapNodes(int dataX, int dataY) {
+    Node prevX = null, currX = root;
+    while (currX != null && currX.getData() != dataX) {
+      prevX = currX;
+      currX = currX.getNext();
+    }
+    
+    Node prevY = null, currY = root;
+    while (currY != null && currY.getData() != dataY) {
+      prevY = currY;
+      currY = currY.getNext();
+    }
+    
+    if(currX == null || currY == null) {
+      System.out.println("No matching nodes to swap. Exiting");
+      return;
+    }
+    
+    if (prevX != null) {
+      prevX.setNext(currY);
+    } else {
+      root = currY;
+    }
+    
+    if (prevY != null) {
+      prevY.setNext(currX);
+    } else {
+      root = currX;
+    }
+    
+    //Swap next pointers
+    Node temp = currX.getNext();
+    currX.setNext(currY.getNext());
+    currY.setNext(temp);
+  }
+  
+  private Node reverse(Node node) {
+    Node prev = null;
+    Node current = node;
+    Node next = null;  //Just a holder
+    while (current != null) {
+      next = current.getNext();
+      current.setNext(prev);
+      prev = current;
+      current = next;
+    }
+    node = prev;
+    return node;
+  }
+  
+  public void reverse() {
+    root = reverse(root);
+  }
+  
+  public Node sortedMerge(Node a, Node b) {
+    Node resultRoot = new Node(Integer.MIN_VALUE);
+    Node currX = a;
+    Node currY = b;
+    Node resultNode = resultRoot;
+    while (currX != null && currY != null) {
+      if (currX.getData() < currY.getData()) {
+        resultNode.setNext(currX);
+        currX = currX.getNext();
+        
+      } else {
+        resultNode.setNext(currY);
+        currY = currY.getNext();
+      }
+      resultNode = resultNode.getNext();
+    }
+    if (currX != null) {
+      resultNode.setNext(currX);
+      currX = currX.getNext();
+      resultNode = resultNode.getNext();
+    }
+    if (currY != null) {
+      resultNode.setNext(currY);
+      currY = currY.getNext();
+      resultNode = resultNode.getNext();
+    }
+    return resultRoot;
+  }
+  
+  public Node sortedMerge(Node b) {
+    Node a = this.root;
+    return sortedMerge(a, b);
+  }
+  
   public static void main(String[] args) {
     LinkedList lList = new LinkedList();
     lList.add(10);
@@ -65,6 +180,41 @@ public class LinkedList {
     System.out.println(lList);
     lList.delete(21);
     System.out.println(lList);
+    lList.delete(20);
+    System.out.println(lList);
+    
+    lList.add(10);
+    lList.add(20);
+    lList.add(30);
+    lList.add(5);
+    lList.add(31);
+    lList.add(23);
+    lList.add(40);
+    lList.deleteAtPos(2);
+    System.out.println(lList);
+    lList.deleteAtPos(0);
+    System.out.println(lList);
+    System.out.println(lList.length());
+    
+    lList.swapNodes(20, 31);
+    System.out.println(lList);
+    
+    lList.reverse();
+    System.out.println(lList);
+    
+    LinkedList a = new LinkedList();
+    a.add(10);
+    a.add(20);
+    a.add(30);
+    
+    LinkedList b = new LinkedList();
+    b.add(11);
+    b.add(25);
+    b.add(27);
+    b.add(31);
+    
+    Node sortedMerged = a.sortedMerge(b.root);
+    System.out.println(sortedMerged);
   }
   
 
